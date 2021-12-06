65f6a4c81170acc51c04229f9fd2628375c59b6d
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-12-06 14:47:33 +0300

    adding ABP

---
+++ MyCompanyName.MyProjectName/MyProjectNameModule.cs
@@ -0,0 +1,32 @@
+﻿using Volo.Abp;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace MyCompanyName.MyProjectName;
+
+[DependsOn(
+    typeof(AbpAspNetCoreModule),
+    typeof(AbpAutofacModule)
+    )]
+public class MyProjectNameModule : AbpModule
+{
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseConfiguredEndpoints();
+    }
+}
--- MyCompanyName.MyProjectName/Program.cs
+++ MyCompanyName.MyProjectName/Program.cs
@@ -1,6 +1,21 @@
+using MyCompanyName.MyProjectName;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseAutofac();
+
+builder.Services.ReplaceConfiguration(builder.Configuration);
+builder.Services.AddApplication<MyProjectNameModule>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/todo", () => new List<string>
+{
+    "Todo 1",
+    "Todo 2"
+});
+
+app.InitializeApplication();
 app.Run();
