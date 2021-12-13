9f6c88ebb0da15f2c30a8a1dcecbc1cc3e8e3f6d
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-12-13 12:49:07 +0300

    add dataSeeder middleware

--- MyCompanyName.MyProjectName/MyProjectNameModule.cs
+++ MyCompanyName.MyProjectName/MyProjectNameModule.cs
@@ -247,6 +247,8 @@ public class MyProjectNameModule : AbpModule
             app.UseErrorPage();
         }
 
+        app.UseMiddleware<DbSeederMiddleware>();
+        
         app.UseCorrelationId();
         app.UseStaticFiles();
         app.UseRouting();
